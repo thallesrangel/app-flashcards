db4c779ce29df60dbf27968576b3abe507142c11
Thalles Rangel <rangelthr@gmail.com>
2025-05-06 23:48:53 -0300

    mexendo no css

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <a class="navbar-brand" href="{{ route('flashcard') }}">
             <img id="logo-img" class="img-fluid" src="{{ asset('logo.png') }}"/>
-            <span class="text-dark fw-bold fs-5">New Lang</span>
+            <span class="text-dark fw-bold fs-5">Lang HUB</span>
         </a>
     </div>
 </nav>
